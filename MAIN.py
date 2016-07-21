f580a7c6d310263f6b09c0f4320a0dd28b59d852
Dylan Bruss <Dylan Bruss>
2016-07-21 19:12:13 -0400

    fixed 'open with' issue now you can open all of your text files with Cyntacs

---
+++ MAIN.py
@@ -0,0 +1,207 @@
+from tkinter import *
+from tkinter.filedialog import *
+import os
+oldpath = os.getcwd()
+from SyntaxHighlighting import *
+import sys
+import time
+
+# not quite sure what this does yet...
+fileName = "New"
+loadText = ""
+load2 = False
+
+class App(Tk):
+    def __init__(self):
+        os.chdir(str(os.path.realpath(sys.argv[0])).replace("MAIN.exe","").replace("MAIN.py",""))
+        Tk.__init__(self)
+
+        #basic variables
+        self.saveFile = "New" #this stores the file name of the currently open file
+
+        self.synHlght = syntaxHighlighter()
+        #widgets
+        self.scrollbar = Scrollbar(self)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.numbers = Text(self, bg = "lightgrey", width = 4, state=DISABLED)
+        self.numbers.pack(side = LEFT, anchor="nw", fill=Y)
+
+        self.textArea = Text(self, wrap = NONE, maxundo = 30, undo = 1,tabs="    ")
+        self.textArea.pack(fill = BOTH, expand=1)
+        self.current = self.textArea.get("0.0",END)
+
+        self.iconbitmap("Logo2.ico")
+        
+        """if len(sys.argv) > 1:
+            self.saveFile = sys.argv[1]
+            self.title(string = "Cyntacs - "+str(self.saveFile))
+            file = open(sys.argv[1])
+            self.textArea.delete("0.0",END)
+            self.textArea.insert("0.0",file.read())
+            self.current = self.textArea.get("0.0",END)
+            self.updateNumbers();
+            self.synHlght.highlightSyntax(self.textArea, self.saveFile)"""
+        if len(sys.argv) > 1:
+            print("opening a file passed as an argument...");
+            os.chdir(oldpath);
+            self.saveFile = sys.argv[len(sys.argv)-1]
+            self.title(string = "Cyntacs - "+str(self.saveFile))
+            file = open(sys.argv[len(sys.argv)-1])
+            os.chdir(str(os.path.realpath(sys.argv[0])).replace("MAIN.exe","").replace("MAIN.py",""))
+            self.textArea.delete("0.0",END)
+            self.textArea.insert("0.0",file.read())
+            self.current = self.textArea.get("0.0",END)
+            self.updateNumbers();
+            self.synHlght.highlightSyntax(self.textArea, self.saveFile)
+        
+
+        def scrollAll(a1=None, a2=None):
+            self.scrollbar.set(a1,a2);
+            currentScroll = self.scrollbar.get()
+            self.numbers.yview_moveto(currentScroll[0])
+            self.textArea.yview_moveto(currentScroll[0])
+
+        def dualscroll(a1=None,a2=None,a3=None,a4=None):
+            self.textArea.yview(a1,a2,a3,a4);
+            self.numbers.yview(a1,a2,a3,a4);
+
+        self.scrollbar.config(command=dualscroll)#connect the scrollbar
+        self.textArea.config(yscrollcommand = scrollAll, maxundo = 20)
+        self.numbers.config(yscrollcommand = scrollAll)
+
+        #menu
+        self.menubar = Menu(self)
+        filemenu = Menu(self.menubar, tearoff=0)
+        #'file' cascade
+        filemenu.add_command(label="Open", command=self.openFile)
+        self.bind("<Control-o>", lambda e: self.openFile())
+        filemenu.add_command(label="Save", command=self.fileSave)
+        self.bind("<Control-s>", lambda e: self.fileSave())
+        filemenu.add_command(label="Save As", command=self.saveAsFile)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.exitCyntacs)
+        self.menubar.add_cascade(label="File", menu=filemenu)
+        #'edit' cascade
+        editmenu = Menu(self.menubar, tearoff=0)
+        editmenu.add_command(label="Undo", command=self.undo)
+        self.bind("<Control-z>", lambda e: self.undo())
+        editmenu.add_command(label="Redo", command=self.redo)
+        self.bind("<Control-y>", lambda e: self.redo())
+        self.menubar.add_cascade(label = "Edit", menu = editmenu)
+        #'help' cascade
+        helpmenu = Menu(self.menubar, tearoff=0)
+        helpmenu.add_command(label="about", command=self.aboutDialog);
+        self.menubar.add_cascade(label = 'Help', menu = helpmenu);
+        #add menubar to window
+        self.config(menu = self.menubar)
+        
+
+        self.title(string = "Cyntacs - "+str(self.saveFile)) #changes the title
+
+        self.file_opt = options = {}
+        options['filetypes'] = [('all files', '.*'), ('text files', '.txt'), ('python script','.py .pyw'), ('HTML (Hyper-Text Markup Language) file','.html .htm')]
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = self
+
+        self.syntaxTimer = 0;
+        
+        self.after(10, self.checkModified)
+        
+        self.mainloop()
+
+    def undo(self):
+        try:
+            self.textArea.edit_undo()
+        except:
+            pass
+    def redo(self):
+        try:
+            self.textArea.edit_redo()
+        except:
+            pass
+    def aboutDialog(self):
+        warn = Toplevel(master = self)
+        warn.title(string="About Cyntacs")
+        warn.resizable(width = False, height = False)
+        warn.transient(master = self)
+        Label(warn, text = "Cyntacs\nversion: 0.5\n\n(c) Coderman64 2016\n http://coderman64.github.io/").pack()
+        Button(warn, text = "OK", command = warn.destroy, default=ACTIVE, width=10).pack()
+        warn.grab_set()
+        self.focus_set()
+        self.wait_window(warn)
+    def openFile(self):
+        dialogResult = askopenfilename(**self.file_opt)
+        if dialogResult != "":
+            try:
+                self.saveFile = dialogResult
+                self.title(string = "Cyntacs - "+str(self.saveFile))
+                file = open(dialogResult)
+                self.textArea.delete("0.0",END)
+                self.textArea.insert("0.0",file.read())
+                self.current = self.textArea.get("0.0",END)
+            except FileNotFoundError:
+                warn = Toplevel(master = self)
+                warn.title(string="ERROR")
+                warn.resizable(width = False, height = False)
+                warn.transient(master = self)
+                warn.grab_set()
+                Label(warn, text = "File not found:\n %s" % dialogResult).pack()
+                Button(warn, text = "OK", command = warn.destroy, default=ACTIVE, width=10).pack()
+                self.wait_window(warn)
+        self.updateNumbers();
+        self.synHlght.highlightSyntax(self.textArea, self.saveFile)
+    def fileSave(self):
+        if self.saveFile == "New":
+            self.saveAsFile()
+        else:
+            try:
+                file = open(self.saveFile, mode='w')
+            except FileNotFoundError:
+                file = open(self.saveFile, mode='x')
+            file.write(self.current)
+            self.title(string = "Cyntacs - "+str(self.saveFile))
+    def saveAsFile(self):
+        dial = asksaveasfilename(**self.file_opt)
+        if dial != "":
+            self.saveFile = dial
+            self.title(string = "Cyntacs - "+str(self.saveFile))
+            try:
+                file = open(dial, mode='w')
+            except FileNotFoundError:
+                file = open(dial, mode='x')
+            file.write(self.textArea.get("0.0",END))
+        self.updateNumbers()
+    def exitCyntacs(self):
+        self.quit()
+        self.destroy()
+    def checkModified(self):
+        fileName = self.saveFile;
+        now = self.textArea.get("0.0",END)
+        if now != self.current:
+            self.title(string = "* Cyntacs - "+str(self.saveFile)+" *")# changes the title if the file is not saved
+            self.current = now
+            self.updateNumbers()
+            self.textArea.see(INSERT)
+            self.numbers.see(self.textArea.index(INSERT))
+            self.syntaxTimer = 0;
+            self.synHlght.threadOpen12 = False;
+        if self.syntaxTimer == 100:
+            # this is supposed to call the syntax highlighter a full second after a user stops typing
+            # this is supposed to prevent problems that result from the user editing the text while the syntax highlighter is running
+            print("Activating Highlighter");
+            self.synHlght.highlightSyntax(self.textArea, self.saveFile)
+        self.syntaxTimer += 1;
+        self.after(10, self.checkModified)#calls itself again after 10 milliseconds
+    def updateNumbers(self):
+        now = self.textArea.get("0.0",END)
+        self.numbers.config(state = NORMAL)
+        self.numbers.delete("0.0",END);
+        for i in range(0, str(now).count("\n")):
+            if i > 0:
+                self.numbers.insert(END,"\n")
+            self.numbers.insert(END,str(i+1))
+        self.numbers.config(state = DISABLED);
+
+root = App()
+time.sleep(1);
--- SyntaxHighlighting.py
+++ SyntaxHighlighting.py
@@ -1,46 +1,146 @@
 from tkinter import *
 import keyword
+import threading
+import os
+import os.path
+import sys
 
 defLists = {}
+os.chdir(os.path.realpath(sys.argv[0]).replace("MAIN.exe","").replace("MAIN.py",""))
 
-for i in ["keyword","builtin"]:
-    o12 = i+": "
-    syntaxDefinitionFile = open("SyntaxDefs.txt")
-    synDef = syntaxDefinitionFile.read()
-    index1 = synDef.find(o12)
-    keyDef = synDef[index1:]
-    index2 = keyDef.find("\n")
-    keyDefList = keyDef[len(o12):index2].split(',')
-    #print(keyDefList)
-    defLists[i] = keyDefList
+syntaxDefinitionFile = open("SyntaxDefs.txt")
+synDef = syntaxDefinitionFile.read()
+"""
+structure:
+{".filetype":[("color",[items]),("color",[items])]}
+"""
+syntaxDefs = {}
+for ruleset in synDef.split("["):
+    index = ruleset.find(']')
+    filetypes = ruleset[:index].split(',')
+    rules = []
+    number = 0
+    for rule in ruleset[index:].split('\n'):
+        if rule.startswith(">") != -1:
+            index2 = rule.find(":")
+            color12 = rule[1:index2]
+            syntax = rule[index2+1:].split(",")
+            rules.append((color12,syntax));
+            number += 1
+    for ft in filetypes:
+        ft = ft.strip(" ")
+        syntaxDefs[ft] = rules;
+        print(str(ft)+": "+str(rules))
 
-def highlightSyntax(text, filename):
-    if filename.endswith(".py") or filename.endswith(".pyw"):
-        text.tag_delete("keyword")
-        text.tag_delete("builtin")
-        text.tag_configure("keyword", foreground="orange")
-        text.tag_configure("builtin", foreground="purple")
-        for s in defLists["keyword"]:
-            s = s.strip()
-            start = 1.0
-            while 1:
-                pos = text.search(s, start, stopindex=END)
-                if not pos:
-                    break
-                else:
-                    if text.get(pos+"-1c",pos+"+"+str(len(s)+1)+"c").strip(" \n\t:") == str(s):
-                        text.tag_add("keyword",pos,pos+"+"+str(len(s))+"c")
-                #print(pos)
-                start = pos + "+1c"
-        for s in defLists["builtin"]:
-            s = s.strip()
-            start = 1.0
-            while 1:
-                pos = text.search(s, start, stopindex=END)
-                if not pos:
-                    break
-                else:
-                    if text.get(pos+"-1c",pos+"+"+str(len(s)+1)+"c").strip(" +=\n\t:(.") == str(s):
-                        text.tag_add("builtin",pos,pos+"+"+str(len(s))+"c")
-                #print(pos)
-                start = pos + "+1c"
+class syntaxHighlighter:
+    def __init__(self):
+        self.thread12 = None
+        self.threadOpen12 = False
+    def highlightSyntax(self, text, filename):
+        if self.threadOpen12 == False:
+            self.thread12 = threading.Thread(target = lambda: self.highlightSyntaxSub(text, filename))
+            self.thread12.start()
+            self.threadOpen12 = True
+
+    def highlightSyntaxSub(self, text, filename):
+        index = filename.rfind(".")
+        print("hi")
+        try:
+            syntaxDefs[str(filename[index:])]
+            for rule in syntaxDefs[str(filename[index:])]:
+                text.tag_delete(rule[0])
+                text.tag_configure(rule[0], foreground=rule[0])
+                #print(rule[1])
+                for item in rule[1]:
+                    item = item.strip()
+                    start = 1.0
+                    if item.find("+*+") != -1:
+                        try:
+                            while 1:
+                                pos2 = "1.0"
+                                pos = text.search(item[:item.find("+")], start, stopindex=END)
+                                if not pos:
+                                    break
+                                else:
+                                    pos2 = text.search(item[item.rfind("+")+1:].replace("\\n","\n"), pos+"+1c", stopindex=END)
+                                    if pos2 != "":
+                                        text.tag_add(rule[0],pos,pos2+"+"+str(len(item[item.rfind("+")+1:].replace("\\n","\n")))+"c")
+                                #print(pos)
+                                start = pos2 + "+1c"
+                                if self.threadOpen12 == False:
+                                    break
+                                    print("thread Ending 1a");
+                        except:
+                            pass
+                    elif item.find("-*-") != -1:
+                        try:
+                            while 1:
+                                pos2 = "1.0"
+                                pos = text.search(item[:item.find("-")], start, stopindex=END)
+                                if not pos:
+                                    break
+                                else:
+                                    pos2 = text.search(item[item.rfind("-")+1:].replace("\\n","\n"), pos+"+1c", stopindex=END)
+                                    if pos2 != "":
+                                        text.tag_add(rule[0],pos+"+"+str(len(item[:item.find("-")]))+"c",pos2)
+                                #print(pos)
+                                start = pos2 + "+1c"
+                                if self.threadOpen12 == False:
+                                    break
+                                    print("thread Ending 1b");
+                        except:
+                            pass
+                    else:
+                        try:
+                            while 1:
+                                pos = text.search(item, start, stopindex=END)
+                                if not pos:
+                                    break
+                                else:
+                                    if text.get(pos+"wordstart",pos+"wordend") == str(item):
+                                        text.tag_add(rule[0],pos,pos+"wordend")
+                                #print(pos)
+                                start = pos + "+1c"
+                                if self.threadOpen12 == False:
+                                    break
+                                    print("thread Ending 1c");
+                        except:
+                            pass
+                    if self.threadOpen12 == False:
+                        break
+                        print("thread Ending 2");
+                
+        except KeyError:
+            print("dunno filetype: "+str(filename[index:]))
+        print("Highlighting Finished");
+        self.threadOpen12 = False
+    """text.tag_delete("keyword")
+    text.tag_delete("builtin")
+    text.tag_configure("keyword", foreground="orange")
+    text.tag_configure("builtin", foreground="purple")
+    for s in defLists["keyword"]:
+        s = s.strip()
+        start = 1.0
+        while 1:
+            pos = text.search(s, start, stopindex=END)
+            if not pos:
+                break
+            else:
+                if text.get(pos+"-1c",pos+"wordend").strip(" \n\t:") == str(s):
+                    text.tag_add("keyword",pos,pos+"+"+str(len(s))+"c")
+            #print(pos)
+            start = pos + "+1c"
+    for s in defLists["builtin"]:
+        s = s.strip()
+        start = 1.0
+        while 1:
+            pos = text.search(s, start, stopindex=END)
+            if not pos:
+                break
+            else:
+                if text.get(pos+"-1c",pos+"wordend").strip(" +=\n\t:(.") == str(s):
+                    text.tag_add("builtin",pos,pos+"+"+str(len(s))+"c")
+            #print(pos)
+            start = pos + "+1c"""
+              
+
